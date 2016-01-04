ac89aeb89942b8cc2773fad6261247566755139e
Onur Akpolat <onur.akpolat@gmail.com>
2016-01-04 11:24:53 +0100

    Create project files with Tapglue installation

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '9.2'
+use_frameworks!
+
+target 'ios-quickstart' do
+  pod 'Tapglue', '~> 1.1'
+end
---
+++ ios-quickstart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-quickstart
+//
+//  Created by Onur Akpolat on 04/01/16.
+//  Copyright © 2016 Tapglue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
